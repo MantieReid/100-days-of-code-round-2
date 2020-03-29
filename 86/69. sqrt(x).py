7278e4f20a23721f87fa39ca66b90fd524601584
mantie <mantiereid2@gmail.com>
2020-03-28 23:57:22 -0400

    Create 69. sqrt(x).py

---
+++ 86/69. sqrt(x).py
@@ -0,0 +1,13 @@
+import math
+from math import sqrt
+from self import  self
+
+class Solution:
+  def mySqrt(self, x: int) -> int:
+    x2 = math.sqrt(x)
+    print( int(x2))
+    return int(x2)
+
+
+
+Solution.mySqrt(self,x = 49)
