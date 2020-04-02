012f15236228d92b559a8d9698d789b27dca8b82
mantie <mantiereid2@gmail.com>
2020-04-01 23:57:35 -0400

    Create 191. Number of bits.py
    DAY 90

---
+++ Day 90/191. Number of bits.py
@@ -0,0 +1,11 @@
+import collections
+from self import self
+
+class Solution:
+  def hammingWeight(self, n: int) -> int:
+   bin(n).count('1') # counts the number of binarys one in the int.
+
+
+asdad = 0o000000000001011
+
+Solution.hammingWeight(self,n = asdad)
